44e340b8d8cdc13e98ac800f7f66e5f5d128702a
Kristopher Kath <Kristopher.Kath@gmail.com>
2022-02-12 20:49:45 -0800

    Finished Observer pattern.
    Created Egg class that notifies Event that an action has occured. The Event class then notifies all Listeners registered to event where they invoke a UnityEvent action to occur.

---
+++ Assets/Radar/Egg.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Egg : MonoBehaviour
+{
+    public Event dropped;
+    public Image icon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropped.Occurred(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
